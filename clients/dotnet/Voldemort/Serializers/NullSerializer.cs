21e91d7526f01075753561322b97b17073147cec
Jeremy Custenborder <jcustenborder@gmail.com>
2010-10-25 18:17:46 -0500

    Added NullSerializer.cs. This serializer passes in a bytes and returns bytes without change.

---
+++ clients/dotnet/Voldemort/Serializers/NullSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Voldemort.Serializers
+{
+    public class NullSerializer:Serializer<byte[]>
+    {
+        private NullSerializer() { }
+        public static readonly Serializer<byte[]> Instance = new NullSerializer();
+        
+        public byte[] Serialize(byte[] value)
+        {
+            return value;
+        }
+
+        public byte[] Deserialize(byte[] value)
+        {
+            return value;
+        }
+    }
+}
